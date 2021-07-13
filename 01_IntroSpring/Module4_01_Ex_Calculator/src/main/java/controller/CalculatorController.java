752a64a9e79fb131cacd08af7709d00bc48ca4e6
phuonganh1992 <phuonganhkt.1992@gmail.com>
2021-07-13 12:13:19 +0700

    done calculator

--- 01_IntroSpring/Module4_01_Ex_Calculator/src/main/java/controller/CalculatorController.java
+++ 01_IntroSpring/Module4_01_Ex_Calculator/src/main/java/controller/CalculatorController.java
@@ -3,29 +3,35 @@ package controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class CalculatorController {
     @GetMapping("/calculator")
-    public void calculateByCase(@RequestParam float firstOperand, @RequestParam float secondOperand, @RequestParam String operator, Model model){
+    public String showForm(){
+        return "index";
+    }
+
+    @PostMapping("/calculator")
+    public String calculate(@RequestParam float firstOperand, @RequestParam float secondOperand, @RequestParam String operator, Model model){
         model.addAttribute("firstOperand", firstOperand);
         model.addAttribute("secondOperand", secondOperand);
         model.addAttribute("operator",operator);
         float result= calculateByCase(firstOperand, secondOperand, operator);
         model.addAttribute("result",result);
-
+        return "result";
     }
 
     public static float calculateByCase(float firstOperand, float secondOperand, String operator){
         switch (operator){
-            case "+":
+            case "add":
                 return firstOperand+secondOperand;
-            case "-":
+            case "sub":
                 return firstOperand-secondOperand;
-            case "*":
+            case "mul":
                 return firstOperand*secondOperand;
-            case "/":
+            case "divide":
                 if(secondOperand!=0) {
                     return firstOperand / secondOperand;
                 } else throw new RuntimeException("Cant not divide by zero");
--- 01_IntroSpring/Module4_01_Ex_Calculator/src/main/webapp/WEB-INF/views/result.jsp
+++ 01_IntroSpring/Module4_01_Ex_Calculator/src/main/webapp/WEB-INF/views/result.jsp
@@ -2,7 +2,7 @@
   Created by IntelliJ IDEA.
   User: Admin
   Date: 7/13/2021
-  Time: 9:11 AM
+  Time: 12:06 PM
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
@@ -11,29 +11,33 @@
     <title>Title</title>
 </head>
 <body>
-<h1>Simple Calculator</h1>
 <form action="/calculator" method="post">
     <fieldset>
         <legend>Calculator</legend>
         <table>
             <tr>
                 <th>First operand</th>
-                <th><input type="text" name="firstOperand"></th>
+                <th><input type="text" name="firstOperand" value="${firstOperand}"></th>
             </tr>
             <tr>
                 <th>Operator</th>
-                <th><input type="text" name="operator""></th>
+                <th><input type="text" name="operator" value="${operator}"></th>
             </tr>
             <tr>
                 <th>Second operand</th>
-                <th><input type="text" name="secondOperand""></th>
+                <th><input type="text" name="secondOperand" value="${secondOperand}"></th>
             </tr>
             <tr>
                 <th></th>
                 <th><input type="submit" id="submit"></th>
             </tr>
+            <tr>
+                <th>Result</th>
+                <th><input type="text" id="result" value="${result}"></th>
+            </tr>
         </table>
     </fieldset>
 </form>
+
 </body>
 </html>
