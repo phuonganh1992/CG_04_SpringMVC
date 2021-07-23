f6304b11b236bcf4419190e72440e76fbb5b2e69
phuonganh1992 <phuonganhkt.1992@gmail.com>
2021-07-23 10:09:10 +0700

    validate user

--- 08_Validation/Module4_08_Practice_validate-infor-user/src/main/java/com/codegym/controller/UserController.java
+++ 08_Validation/Module4_08_Practice_validate-infor-user/src/main/java/com/codegym/controller/UserController.java
@@ -1,7 +1,31 @@
 package com.codegym.controller;
 
+import com.codegym.model.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class UserController {
+    @ModelAttribute("user")
+    public User initUser(){
+        return new User();
+    }
+
+    @GetMapping("/user")
+    public ModelAndView showForm() {
+        ModelAndView modelAndView = new ModelAndView("/index");
+        return modelAndView;
+    }
+
+    @PostMapping("/validateUser")
+    public ModelAndView checkValidation(@Validated @ModelAttribute("user") User user, BindingResult bindingResult){
+        if(bindingResult.hasErrors()){
+            return new ModelAndView("/index");
+        } else return new ModelAndView("/result");
+    }
 }
