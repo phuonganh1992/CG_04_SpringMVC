68c989847a48743098fdd1fcd17abf955246ee91
phuonganh1992 <phuonganhkt.1992@gmail.com>
2021-07-30 15:44:10 +0700

    done controller

---
+++ Exam_Middle/src/main/java/com/codegym/service/product/ProductService.java
@@ -0,0 +1,40 @@
+package com.codegym.service.product;
+
+import com.codegym.model.Product;
+import com.codegym.repository.IProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Service;
+import org.springframework.data.domain.Pageable;
+import java.util.Optional;
+
+@Service
+public class ProductService implements IProductService{
+    @Autowired
+    private IProductRepository productRepository;
+
+    @Override
+    public Iterable<Product> findAll() {
+        return productRepository.findAll();
+    }
+
+    @Override
+    public Page<Product> findAll(Pageable pageable) {
+        return productRepository.findAll(pageable);
+    }
+
+    @Override
+    public Optional<Product> findById(Long id) {
+        return productRepository.findById(id);
+    }
+
+    @Override
+    public void save(Product product) {
+        productRepository.save(product);
+    }
+
+    @Override
+    public void delete(Long id) {
+        productRepository.deleteById(id);
+    }
+}
